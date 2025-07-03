3e6163eb8f5ee4d80c916e483189954738e3c338
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-07-03 14:39:23 -0300

    feat: :tada: criação e configuração inicial do projeto

---
+++ src/const.ts
@@ -0,0 +1,8 @@
+// REGEX
+export const PASSWORD_REGEX_PATTERN =
+    /^[^ÁáÂâÀàÅåÃãÄäÉéÊêÈèËëÍíÎîÌìÏïÓóÔôÒòÕõÖöÚúÛûÙùÜüÇçÑñÝý]*$/;
+
+// VALIDATION MESSAGE
+export const GENERIC_INPUT_ERROR_MESSAGE = "Preenchimento incorreto";
+export const REQUIRED_INPUT_ERROR_MESSAGE = "Campo obrigatório";
+export const PASSWORD_ERROR_MESSAGE = "A senha deve ser sem acento";
