722f125565512bb9102b0e0b17adb2b99dc278ca
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-07-03 15:31:12 -0300

    refactor: :recycle: remove componente Navbar e sua importação da página de dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import { useUserStore } from "@/stores/useUserStore";
-import Navbar from "@/components/ui/Navbar";
 
 export default function Dashboard() {
     const user = useUserStore((state) => state.user);
@@ -26,7 +25,6 @@ export default function Dashboard() {
 
     return (
         <>
-            <Navbar user={user} />
             <div className="min-h-screen bg-gray-50">
                 <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
                     <div className="bg-white shadow rounded-lg">
