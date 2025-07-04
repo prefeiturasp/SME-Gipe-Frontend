892078995bd0f6d3b3e2ddda00581b3ebfd3e99e
ollyvergithub <ollyverottoboni@gmail.com>
2025-07-04 08:33:31 -0300

    feat: :wrench: Altera middleware para remover rota login inexistente

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,8 +21,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: [
-        "/login/:path*",
-        "/((?!api|_next/static|_next/image|favicon\\.ico|images).*)",
-    ],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
