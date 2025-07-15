c9f43c30298a259924c704b11c878af5e38ff02f
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2025-07-15 07:51:34 -0300

    Revert "feat: :recycle: Altera login para lidar com erros vindos do Back"

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,40 +1,34 @@
-import { useMutation } from '@tanstack/react-query';
-import { loginAction} from '@/actions/login';
-import {
-    LoginRequest,
-} from '@/types/login';
-
-import { useRouter } from 'next/navigation';
+import { useMutation } from "@tanstack/react-query";
 import { useUserStore } from "@/stores/useUserStore";
+import { useRouter } from "next/navigation";
+import { loginAction } from "@/actions/login";
 
-
-
-export default function useLogin() {
-        const setUser = useUserStore((state) => state.setUser);
+const useLogin = () => {
+    const setUser = useUserStore((state) => state.setUser);
     const router = useRouter();
 
-    return useMutation<any, Error, LoginRequest>({
-        mutationFn: async (credentials: LoginRequest) => {
-            const response = await loginAction(credentials);
-            return response;
-        },
-        onSuccess: (data) => {
-            console.log('Login realizado com sucesso:', data);
+    return useMutation({
+        mutationFn: loginAction,
+        onSuccess: (response) => {
+            console.log(
+                "-----------------------------------Resposta do login hook useLogin:",
+                response
+            );
+            if (!response.success) return;
 
-            // Aqui você pode armazenar o token/dados do usuário se necessário
-            // Por exemplo: localStorage.setItem('authToken', data.token);
+            const { name, email, cargo } = response.data;
 
-            // Redirecionar para dashboard ou página principal
+            if (!name || !email || !cargo?.nome) return;
+
             setUser({
-                nome: data.nome,
-                email: data.email,
-                cargo: data.cargo.nome,
+                nome: name,
+                email,
+                cargo: cargo.nome,
             });
 
             router.push("/dashboard");
         },
-        onError: (error) => {
-            console.error('Erro na mutação de login:', error);
-        },
     });
-}
+};
+
+export default useLogin;
