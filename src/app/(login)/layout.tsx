ce209dca31ba7b6c47a4d2e902f5c61fd4c8219f
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-07-28 14:33:24 -0300

    feat: :recycle: refatora layout de login para incluir componente Banner e melhorar a estrutura

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -1,11 +1,21 @@
-export default function LoginLayout({
-    children,
-}: {
+import Banner from "@/components/login/Banner";
+import React from "react";
+
+interface AuthLayoutProps {
     children: React.ReactNode;
-}) {
+}
+
+export default function LoginLayout({ children }: AuthLayoutProps) {
     return (
-        <main className="min-h-screen flex items-center justify-center bg-background">
-            {children}
-        </main>
+        <div className="flex w-screen h-screen overflow-hidden">
+            <div className="w-1/2 h-full min-h-0 flex-shrink-0">
+                <Banner />
+            </div>
+            <div className="w-1/2 h-full min-h-0 flex flex-col bg-white overflow-y-auto justify-center">
+                <div className="w-full flex flex-col items-center flex-shrink-0 px-4 py-8">
+                    {children}
+                </div>
+            </div>
+        </div>
     );
 }
