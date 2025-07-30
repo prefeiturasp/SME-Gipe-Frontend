1b6be5110953c15e3a1ff5160c1e95f7934b209f
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-07-30 17:30:22 -0300

    refactor: :sparkles: refatora componente QuadroUsuario para utilizar o estado do usuário do store

--- src/components/dashboard/MeusDados/QuadroUsuario.tsx
+++ src/components/dashboard/MeusDados/QuadroUsuario.tsx
@@ -1,24 +1,22 @@
+"use client";
+
 import React from "react";
 import User from "@/assets/icons/User";
+import { useUserStore } from "@/stores/useUserStore";
 
-interface QuadroUsuarioProps {
-    nome: string;
-    cpf: string;
-    rf: string;
-}
-
-const QuadroUsuario: React.FC<QuadroUsuarioProps> = ({ nome, cpf, rf }) => {
+const QuadroUsuario: React.FC = () => {
+    const user = useUserStore((state) => state.user);
     return (
         <div className="flex flex-col items-center justify-center bg-[#E8F0FE] rounded-[4px] p-6 h-[calc(100%_-_70px)]">
             <User width={48} height={48} fill="#717FC7" />
             <span className="mt-4 text-[24px] font-bold text-[#42474A] text-center">
-                {nome}
+                {user?.nome}
             </span>
             <span className="mt-2 text-[14px] text-[#42474A] text-center">
-                CPF: {cpf}
+                CPF: {user?.cpf}
             </span>
             <span className="text-[14px] text-[#42474A] text-center">
-                RF: {rf}
+                RF: {user?.identificador}
             </span>
         </div>
     );
