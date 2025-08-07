e15303a4288b01cec77cc5d13bf47b553d586d22
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-08-07 17:19:32 -0300

    style: :lipstick: ajusta classes de estilo do componente Input para consistência visual

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -18,9 +18,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             <input
                 type={type}
                 className={cn(
-                    "flex h-10 w-full border border-input bg-background px-3 py-2 text-sm font-medium ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed rounded-[4px]",
-                    error &&
-                        "!border-destructive focus-visible:ring-destructive",
+                    "flex h-10 w-full border border-[#dadada] bg-background px-3 py-2 text-sm font-medium ring-0 ring-offset-0 shadow-none outline-none focus:outline-none focus:ring-0 focus:ring-offset-0 focus:shadow-none focus:bg-[#E8F0FE] focus:border-[#ced4da] disabled:cursor-not-allowed rounded-[4px]",
+                    error && "!border-destructive",
                     className
                 )}
                 ref={ref}
