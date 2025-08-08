68a7534def34f6608074310b4ac275533aff5b11
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-08-08 13:52:03 -0300

    refactor: :white_check_mark: substitui useMutation por useQuery nas funções de busca de DREs e UEs

--- src/hooks/useUnidades.ts
+++ src/hooks/useUnidades.ts
@@ -1,24 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getDREs, getUEs } from "@/actions/unidades";
 
-export function useFetchDREs(
-    setDreOptions: (dres: { uuid: string; nome: string }[]) => void
-) {
-    return useMutation({
-        mutationFn: getDREs,
-        onSuccess: (res: { uuid: string; nome: string }[]) => {
-            setDreOptions(res.map(({ uuid, nome }) => ({ uuid, nome })));
-        },
+export function useFetchDREs() {
+    return useQuery({
+        queryKey: ["get-dres"],
+        queryFn: () => getDREs(),
+        refetchOnWindowFocus: false,
     });
 }
-export function useFetchUEs(
-    dreUuid: string | undefined,
-    setUeOptions: (ues: { uuid: string; nome: string }[]) => void
-) {
-    return useMutation({
-        mutationFn: () => (dreUuid ? getUEs(dreUuid) : Promise.resolve([])),
-        onSuccess: (res: { uuid: string; nome: string }[]) => {
-            setUeOptions(res.map(({ uuid, nome }) => ({ uuid, nome })));
-        },
+
+export function useFetchUEs(dreUuid: string) {
+    return useQuery({
+        queryKey: ["get-ues", dreUuid],
+        queryFn: () => getUEs(dreUuid),
+        enabled: !!dreUuid,
+        refetchOnWindowFocus: false,
     });
 }
