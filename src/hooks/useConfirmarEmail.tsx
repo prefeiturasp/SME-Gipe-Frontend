a8f376cce991c3835b08e89635fa0be52a7acef5
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-09-10 16:54:55 -0300

    feat: :sparkles: adiciona lógica de sucesso na mutação de confirmação de email para atualizar o email do usuário

--- src/hooks/useConfirmarEmail.tsx
+++ src/hooks/useConfirmarEmail.tsx
@@ -1,9 +1,24 @@
 import { useMutation } from "@tanstack/react-query";
 import { confirmarEmailAction } from "@/actions/confirmar-email";
+import { useUserStore } from "@/stores/useUserStore";
 
 const useConfirmarEmail = () => {
+    const setUser = useUserStore((state) => state.setUser);
     return useMutation({
         mutationFn: confirmarEmailAction,
+        onSuccess: (response) => {
+            if (!response.success || !response.new_mail) return;
+
+            const email = response.new_mail;
+
+            const currentUser = useUserStore.getState().user;
+            if (!currentUser) return;
+
+            setUser({
+                ...currentUser,
+                email,
+            });
+        },
     });
 };
 
