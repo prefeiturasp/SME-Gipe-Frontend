9818933c40cc979e0da4c80e616b337edc128d8b
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-09-24 17:27:18 -0300

    feat: :sparkles: adiciona códigos de perfil ao arquivo de constantes

--- src/const.ts
+++ src/const.ts
@@ -6,3 +6,9 @@ export const PASSWORD_REGEX_PATTERN =
 export const GENERIC_INPUT_ERROR_MESSAGE = "Preenchimento incorreto";
 export const REQUIRED_INPUT_ERROR_MESSAGE = "Campo obrigatório";
 export const PASSWORD_ERROR_MESSAGE = "A senha deve ser sem acento";
+
+// PERFIL CODES
+export const PERFIL_GIPE = 0;
+export const PERFIL_PONTO_FOCAL = 1;
+export const PERFIL_ASSISTENTE_DIRETOR = 3085;
+export const PERFIL_DIRETOR = 3360;
