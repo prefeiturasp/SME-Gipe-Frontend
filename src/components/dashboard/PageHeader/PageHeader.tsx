d7e104c5104d9ea43853e23eeb6550da89e22be0
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-09-30 11:32:13 -0300

    feat: :sparkles: adiciona componente PageHeader com funcionalidade de botão Voltar e testes correspondentes

---
+++ src/components/dashboard/PageHeader/PageHeader.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import ArrowLeft from "@/assets/icons/ArrowLeft";
+import { useRouter } from "next/navigation";
+
+interface PageHeaderProps {
+    title: string;
+    showBackButton?: boolean;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({
+    title,
+    showBackButton = true,
+}) => {
+    const router = useRouter();
+    return (
+        <div className="flex items-center justify-between w-full px-4">
+            <h1 className="text-[#42474a] text-[24px] font-bold m-0">
+                {title}
+            </h1>
+            {showBackButton && (
+                <Button variant="customOutline" onClick={() => router.back()}>
+                    <ArrowLeft />
+                    &nbsp;Voltar
+                </Button>
+            )}
+        </div>
+    );
+};
+
+export default PageHeader;
