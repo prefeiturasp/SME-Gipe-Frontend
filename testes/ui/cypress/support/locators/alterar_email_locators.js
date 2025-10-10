9072611ad8883df182276ebe90ec28e275a0c193
Wilson Santos <wilson.santos@spassu.com.br>
2025-10-10 08:52:44 -0300

    Ajustes locators

--- testes/ui/cypress/support/locators/alterar_email_locators.js
+++ testes/ui/cypress/support/locators/alterar_email_locators.js
@@ -1,19 +1,11 @@
 class Alterar_Email_Localizadores {
   botao_alterar_email() {
-    return "/html/body/div[1]/div/div[2]/main/div[2]/div/div[2]/div/div[2]/div/button";
+    return "//button[contains(normalize-space(text()), 'Alterar e-mail')]"; 
     
   }
 
   imput_email() {
     return 'input[placeholder="Digite o seu e-mail corporativo"]';
-  }
-
-  botao_salvar_email() {
-    return '//*[@id="radix-:r3:"]/form/div[2]/button[2]';
-  }
-
-  botao_cancelar() {
-    return '//*[@id="radix-:ra:"]/form/div[1]/div[2]/span';
   }
 }
 
