f82dbc109801ced36e31b01eaec33dcd6b0c987a
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-10-17 17:04:28 -0300

    feat: :sparkles: adiciona testes para a página CadastrarOcorrencia com verificação de elementos principais

---
+++ src/app/dashboard/cadastrar-ocorrencia/[uuid]/page.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import CadastrarOcorrenciaPage from "./page";
+import { vi } from "vitest";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+vi.mock("next/navigation", () => ({
+    useRouter: () => ({
+        back: vi.fn(),
+    }),
+}));
+
+const queryClient = new QueryClient();
+
+const renderWithClient = (ui: React.ReactElement) => {
+    return render(
+        <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>
+    );
+};
+
+describe("CadastrarOcorrenciaPage", () => {
+    it("deve renderizar o componente CadastrarOcorrencia com os elementos principais", () => {
+        renderWithClient(<CadastrarOcorrenciaPage />);
+
+        expect(
+            screen.getByRole("heading", {
+                name: /intercorrências institucionais/i,
+            })
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByRole("heading", { name: /nova ocorrência/i })
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByText(
+                /preencha as informações abaixo para registrar uma nova ocorrência/i
+            )
+        ).toBeInTheDocument();
+
+        expect(screen.getByText("Cadastro de ocorrência")).toBeInTheDocument();
+        expect(screen.getByText("Formulário patrimonial")).toBeInTheDocument();
+        expect(screen.getByText("Fase 03")).toBeInTheDocument();
+        expect(screen.getByText("Anexos")).toBeInTheDocument();
+    });
+});
