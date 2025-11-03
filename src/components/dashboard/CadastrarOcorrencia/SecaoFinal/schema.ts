a7573355bb35eb7437d4b3d143c35b621b532910
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-11-03 12:10:46 -0300

    refactor: :recycle: altera validação do schema da seção final para uso de strings em vez de enums

--- src/components/dashboard/CadastrarOcorrencia/SecaoFinal/schema.ts
+++ src/components/dashboard/CadastrarOcorrencia/SecaoFinal/schema.ts
@@ -1,21 +1,11 @@
 import * as z from "zod";
 
 export const formSchema = z.object({
-    declarante: z.enum(
-        ["Gabinete DRE", "GCM", "GIPE", "NAAPA", "Unidade Educacional"],
-        {
-            required_error: "Selecione o declarante.",
-        }
-    ),
-    comunicacaoSeguranca: z.enum(["Sim, com a GCM", "Sim, com a PM", "Não"], {
-        required_error: "Informe se houve comunicação com a segurança pública.",
-    }),
-    protocoloAcionado: z.enum(
-        ["Ameaça", "Alerta", "Apenas para registro/não se aplica"],
-        {
-            required_error: "Selecione o protocolo acionado.",
-        }
-    ),
+    declarante: z.string().min(1, "Selecione o declarante."),
+    comunicacaoSeguranca: z
+        .string()
+        .min(1, "Informe se houve comunicação com a segurança pública."),
+    protocoloAcionado: z.string().min(1, "Selecione o protocolo acionado."),
 });
 
 export type SecaoFinalData = z.infer<typeof formSchema>;
