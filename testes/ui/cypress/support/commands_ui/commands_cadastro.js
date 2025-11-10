388deaa82c6d9cc69300e57be1616d9e694ac834
Marcus Vinicius Silva da Rocha <marcus.rocha@spassu.com.br>
2025-11-10 13:26:07 -0300

    Remove pasta duplicada testes/ui/ - mantendo arquivos na raiz

--- testes/ui/cypress/support/commands_ui/commands_cadastro.js
+++
@@ -1,7 +0,0 @@
-import Cadastro_Localizadores from '../locators/login_locators'
-
-const cadastroLocalizadores = new Cadastro_Localizadores()
-
-Cypress.Commands.add('cadastro_gipe', () => {
-  cy.visit('/cadastro')
-})
