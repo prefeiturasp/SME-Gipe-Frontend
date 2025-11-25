8a48713cac2d73877c7a47fcaabfb7b766891fc6
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2025-11-25 14:56:26 -0300

    feat: :sparkles: atualiza lógica de navegação no FormularioDre e substitui QuadroBranco por DetalhamentoDre

--- src/components/dashboard/FormularioDre/index.tsx
+++ src/components/dashboard/FormularioDre/index.tsx
@@ -2,8 +2,9 @@
 
 import { useOcorrenciaFormStore } from "@/stores/useOcorrenciaFormStore";
 import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import PageHeader from "../PageHeader/PageHeader";
-import QuadroBranco from "../QuadroBranco/QuadroBranco";
+import { DetalhamentoDre } from "./DetalhamentoDre";
 
 export default function FormularioDrePage() {
     const reset = useOcorrenciaFormStore((state) => state.reset);
@@ -11,37 +12,25 @@ export default function FormularioDrePage() {
         (state) => state.ocorrenciaUuid
     );
     const queryClient = useQueryClient();
+    const router = useRouter();
 
-    const handleClick = async () => {
+    const handleClickBack = async () => {
         reset();
 
         await queryClient.invalidateQueries({
             queryKey: ["ocorrencia", ocorrenciaUuid],
         });
+
+        router.back();
     };
 
     return (
         <div className="pt-4">
             <PageHeader
                 title="Detalhes da Intercorrência - Diretoria Regional de Educação (DRE)"
-                onClickBack={handleClick}
+                onClickBack={handleClickBack}
             />
-            <QuadroBranco>
-                <div className="flex flex-col">
-                    <h1 className="text-[#42474a] text-[24px] font-bold m-0">
-                        Nova ocorrência
-                    </h1>
-                    <span className="text-[14px] text-[#42474a] my-4">
-                        Preencha as informações abaixo para registrar uma nova
-                        ocorrência. Lembre-se de colocar a maior quantidade de
-                        detalhes possíveis para nos ajudar a planejar ações de
-                        prevenção e solução de problemas de segurança.
-                    </span>
-                    <p className="text-[14px] text-[#42474a]">
-                        *Respostas obrigatórias
-                    </p>
-                </div>
-            </QuadroBranco>
+            <DetalhamentoDre />
         </div>
     );
 }
