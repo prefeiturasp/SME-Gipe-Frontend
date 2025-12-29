c8be82cc110a31bd626c2585d614ef0794346380
MatheusL-Dev <darkaixxt@gmail.com>
2025-12-29 15:16:21 -0300

    feat: :sparkles: Implementa regra para reativacao de usuarios inativos

--- src/components/dashboard/GestaoUsuarios/PageHeader/PageHeader.tsx
+++ src/components/dashboard/GestaoUsuarios/PageHeader/PageHeader.tsx
@@ -5,18 +5,23 @@ import { Button } from "@/components/ui/button";
 import ArrowLeft from "@/assets/icons/ArrowLeft";
 import Link from "next/link";
 import UserBlock from "@/assets/icons/UserBlock";
+import { useObterUsuarioGestao } from "@/hooks/useObterUsuarioGestao";
 
 interface PageHeaderProps {
     title: string;
     edit?: boolean;
+    usuarioUuid?: string;
     onClickBack?: () => void;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = ({
     title,
     edit = true,
+    usuarioUuid,
     onClickBack,
 }) => {
+    const { data: usuarioData } = useObterUsuarioGestao({uuid: usuarioUuid || ""});
+
     return (
         <div className="flex items-center justify-between w-full px-4">
             <h1 className="text-[#42474a] text-[20px] font-bold m-0">
@@ -32,6 +37,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({
                             <ArrowLeft />
                         </Link>
                     </Button>
+                    {usuarioData?.is_active ?
                     <Button
                         variant="outlineDestructive"
                         type="button"
@@ -40,6 +46,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({
                         <UserBlock className="mr-2" />
                         Inativar perfil
                     </Button>
+                    : <Button
+                        variant="customOutline"
+                        type="button"
+                        className="text-center rounded-md text-[14px] font-[700]"
+                    >
+                        Reativar perfil
+                    </Button>}
                 </div>
             ) : (
                 <Button asChild variant="customOutline">
