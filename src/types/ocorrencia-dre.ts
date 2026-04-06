48fef06864e119742344f94eb9b5eace5b168723
Guilherme Vilas Boas Roberto <guilherme.vb.roberto@gmail.com>
2026-04-06 12:04:45 -0300

    refactor: :recycle: remove campos desnecessários do schema e da interface OcorrenciaDre

--- src/types/ocorrencia-dre.ts
+++ src/types/ocorrencia-dre.ts
@@ -7,26 +7,14 @@ export interface OcorrenciaDreResponse {
     status_display: string;
     status_extra: string;
     acionamento_seguranca_publica: boolean;
-    interlocucao_sts: boolean;
-    info_complementar_sts: string;
-    interlocucao_cpca: boolean;
-    info_complementar_cpca: string;
     interlocucao_supervisao_escolar: boolean;
-    info_complementar_supervisao_escolar: string;
-    interlocucao_naapa: boolean;
-    info_complementar_naapa: string;
+    nr_processo_sei?: string;
 }
 
 export type OcorrenciaDreBody = {
     unidade_codigo_eol: string;
     dre_codigo_eol: string;
     acionamento_seguranca_publica: boolean;
-    interlocucao_sts: boolean;
-    info_complementar_sts: string;
-    interlocucao_cpca: boolean;
-    info_complementar_cpca: string;
     interlocucao_supervisao_escolar: boolean;
-    info_complementar_supervisao_escolar: string;
-    interlocucao_naapa: boolean;
-    info_complementar_naapa: string;
+    nr_processo_sei?: string;
 };
